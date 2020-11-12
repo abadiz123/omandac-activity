6a74d869df4f2c1b976a50aaaa41508d2843521d
abadiz123 <34390379+abadiz123@users.noreply.github.com>
2020-11-12 20:46:36 +0800

    Update index.php

--- index.php
+++ index.php
@@ -173,7 +173,7 @@ function scrollFunction() {
         gameContent3.style.opacity = "0";
     }
   }else {
-    centerLogo.style.width = "100%";
+    centerLogo.style.width = "638.04px";
     centerLogo.style.left = "calc((100% - 638.04px)/2)";
     centerLogo.style.top = "250px";
     centerContent.style.opacity = "0";
